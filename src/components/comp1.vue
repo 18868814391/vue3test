efced9f912e389ae6e22660adb3a2795c66672cd
DESKTOP-EAKE4SI\Administrator <605729908@qq.com>
2020-09-29 18:00:42 +0800

    v-model

---
+++ src/components/comp1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>I am components 1</div>
+  <input type="text" placeholder="i am in comp1" v-model="data1_c" />
+  <input type="text" placeholder="i am in comp1" v-model="data2_c" />
+</template>
+
+<script>
+export default {
+  setup() {},
+  props: {
+    data1_c: {
+      type: String,
+      default: '',
+    },
+    data2_c: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,16 +2,31 @@
   <div>I am in Contact</div>
   <div>{{ appdata }}</div>
   <button @click="changeFather()">changeFatherData</button>
+  <Comp1 v-model:data1_c="data1" v-model:data2_c="data2"></Comp1>
 </template>
 
 <script>
-import { provide, inject } from 'vue'
+import { provide, inject, ref } from 'vue'
+//申明式引入，为了tree-shaking， Vue 应用程序中未使用的全局 api 将从最终捆绑包中消除
+//nextTick  observable  version compile set  delete
+import { nextTick } from 'vue'
+import Comp1 from '@/components/comp1.vue'
 export default {
   setup() {
     const appdata = inject('appdata')
+    const data1 = ref('123')
+    const data2 = ref('456')
     return {
       appdata,
+      data1,
+      data2,
     }
+  },
+  components: {
+    Comp1,
+  },
+  async created() {
+    await nextTick(console.log(123))
   },
   methods: {
     changeFather() {
